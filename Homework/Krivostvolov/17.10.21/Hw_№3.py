b941f3baf54974caae4d82ab63ecf227ec916044
Krivostvolov <gti5660@gmail.com>
2021-10-17 23:18:29 +0300

    Исправил

--- Homework/Krivostvolov/17.10.21/Hw_№3.py
+++ Homework/Krivostvolov/17.10.21/Hw_№3.py
@@ -1,10 +1,2 @@
-distance = '|-'
-step = '|_'
-print (step)
-print (distance + step)
-print ((distance*2) + step)
-print ((distance*3) + step)
-print ((distance*4) + step)
-print ((distance*5) + step)
-print ((distance*6) + step)
-print ((distance*7) + step)
+txt = 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.'
+print (txt.replace ('.', '@'))
