fa9b49e098813a113e6b46ecc74df920df756f0b
AndrewKhukhrianskyi <55056139+AndrewKhukhrianskyi@users.noreply.github.com>
2021-10-24 19:28:06 +0300

    Update True&False.py

--- Vlad's Homework/True&False.py
+++ Vlad's Homework/True&False.py
@@ -1,8 +1,19 @@
 print('Вы будете считать чи не являеться число1 меньше числа2 ')
-a=int(input("Ведите число1 "))
+a=int(input("Ведите число 1 "))
 b=int(input("Ведите число2 "))
 c=(a<b)
 if c==True:
     print('Yes!')
 else:
     print("No!")
+
+# Best Practice
+# Refactored version
+
+a=int(input("Введите число 1: "))
+b=int(input("Введите число 2: "))
+
+if a < b:
+    print('Yes')
+else:
+    print('No')
