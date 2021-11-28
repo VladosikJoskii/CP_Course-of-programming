459b879f0b06b59f08b9457d7f266029e4725a22
Krivostvolov <gti5660@gmail.com>
2021-11-28 22:36:45 +0200

    Домашка

--- Homework/Krivostvolov/28.11.21/Krivostvolov_Hw №2.py
+++ Homework/Krivostvolov/28.11.21/Krivostvolov_Hw №2.py
@@ -1,8 +1,9 @@
 students = int(input('Введите количество учащихся: '))
 num = 1
+result_mark = 0
 for i in range(0, students, 1):
     mark = int(input(f"Введите оценку ученика {num}: "))
     num += 1
-    mark += mark
-middle = mark // students
+    result_mark += mark
+middle = result_mark / students
 print(f"Средняя оценка учеников : {middle}")
