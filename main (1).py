6d013dc6ca23ba347aa9439dd2df31257953df56
VladosikJoskii <92318233+VladosikJoskii@users.noreply.github.com>
2021-12-27 13:48:15 +0200

    Bot
    Bot

---
+++ main (1).py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a = (1,2,3,4,5,5,4,4,2,2)
+a.index
+a.count(5)
+
+
