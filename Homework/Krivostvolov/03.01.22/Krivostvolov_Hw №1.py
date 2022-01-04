e67bc24fba8e62f33fe11e36e0a93670574d1ca2
Krivostvolov <gti5660@gmail.com>
2022-01-04 12:39:00 +0200

    I`m back

---
+++ Homework/Krivostvolov/03.01.22/Krivostvolov_Hw №1.py
@@ -0,0 +1,5 @@
+num_1 = int(input('Введите число 1: '))
+num_2 = int(input('Введите число 2: '))
+def add(num_1, numn_2):
+    return(num_1 + num_2)
+print(add(num_1, num_2))
---
+++ Homework/Krivostvolov/03.01.22/Krivostvolov_Hw №2.py
@@ -0,0 +1,5 @@
+num_1 = int(input('Введите число 1: '))
+num_2 = int(input('Введите число 2: '))
+def multiply(num_1, numn_2):
+    return(num_1 * num_2)
+print(multiply(num_1, num_2))
